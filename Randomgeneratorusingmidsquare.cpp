b54256e1230dc051625b3b3af6a2fe45872e7f87
Manish Bhattarai <bhaimanish00@gmail.com>
2020-02-13 10:23:41 +0545

    added random generator using mid square

---
+++ Randomgeneratorusingmidsquare.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+	long int seed = 6897 ;
+	int n,random,i;
+	printf("\nHow many number do you want to generate?\t");
+	scanf("%d", &n);
+	
+	printf("\nRandom numbers are :");
+	
+	for(i=0;i<n;i++){
+		seed = seed * seed;
+		seed = seed/100;
+		seed = seed % 10000;
+		random = seed;
+		printf("%d\t",random);
+	}
+	printf("\n");
+	return 0;
+}
